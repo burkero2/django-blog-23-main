c79b884e6d7e329822d9d8629259c2925b0e2132
burkero2 <>
2023-08-29 14:53:11 +0000

    Adding Elephant SQL Database

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,8 @@ SECRET_KEY = 'django-insecure-4!tzgx41pqn&i8_26)io2y*-y_v_y+j^_ihv3-8m5z76@lai7z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-burkero2-django-blog-23-fl9es5r6es.us2.codeanyapp.com','.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-burkero2-django-blog-23-w4d0z02wox.us2.codeanyapp.com', '.herokuapp.com']
 
 
 # Application definition
@@ -74,11 +80,15 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
