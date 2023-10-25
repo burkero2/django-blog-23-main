96f13b1ac933a8e20c39d19b6094732a236fea01
burkero2 <>
2023-10-25 11:35:22 +0000

    Adding AllAuth

--- blog/views.py
+++ blog/views.py
@@ -12,12 +12,15 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 
-def post_detail(request, slug, *args, **kwargs):
+def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
 
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "coder": "Ronan Burke",
+        },
     )
